f4c7a210e71ecadf85312151e33277627dd78401
Majk Shkurti <majkshkurti@outlook.com>
2021-06-13 03:55:49 +0200

    fix bug

--- app/database/initdb.sh
+++ app/database/initdb.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 set -e
 
-function create_user_and_database() {
-	local db=$1
-	echo "  Creating user and database '$db'"
-	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-	    CREATE USER $db;
-		ALTER USER $db with encrypted password '$POSTGRES_PASSWORD';
-	    CREATE DATABASE $db;
-	    GRANT ALL PRIVILEGES ON DATABASE $db TO $db;
-EOSQL
-}
-
 function update_database_with_postgis() {
     local db=$1
     echo "  Updating databse '$db' with extension"
@@ -23,7 +12,6 @@ EOSQL
 
 if [ -n "$POSTGRES_DB_NAME" ]; then
 	echo "Database creation requested: $POSTGRES_DB_NAME"
-	create_user_and_database $POSTGRES_DB_NAME
     update_database_with_postgis $POSTGRES_DB_NAME
 	echo "Database created"
 fi
