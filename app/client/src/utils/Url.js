58afc1219bc87323ad28a8bb37cb56cbdad6bd7f
Majk Shkurti <majkshkurti@outlook.com>
2022-04-16 02:03:52 +0200

    [FIX] ValidUrl

--- app/client/src/utils/Url.js
+++ app/client/src/utils/Url.js
@@ -72,26 +72,30 @@ const UrlUtil = {
    * @return {boolean}             Is Url Valie
    */
   validURL(string) {
-    /* eslint-disable-next-line */
-    var res = string.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
-    return (res !== null)
+    if (!string) {
+      return false;
+    }
+    var res = string.match(
+      /* eslint-disable-next-line */
+      /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g
+    );
+    return res !== null;
   },
 
   /**
-   * 
-   * @param {String} uri 
-   * @param {String} key 
-   * @param {String | Number} value 
-   * @returns 
+   *
+   * @param {String} uri
+   * @param {String} key
+   * @param {String | Number} value
+   * @returns
    */
   updateQueryStringParameter(uri, key, value) {
-    var re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
-    var separator = uri.indexOf('?') !== -1 ? "&" : "?";
+    var re = new RegExp('([?&])' + key + '=.*?(&|$)', 'i');
+    var separator = uri.indexOf('?') !== -1 ? '&' : '?';
     if (uri.match(re)) {
-      return uri.replace(re, '$1' + key + "=" + value + '$2');
-    }
-    else {
-      return uri + separator + key + "=" + value;
+      return uri.replace(re, '$1' + key + '=' + value + '$2');
+    } else {
+      return uri + separator + key + '=' + value;
     }
   }
 };
