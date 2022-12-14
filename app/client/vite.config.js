677b942445890f40d94602e15b046939771a64b5
GRS80 <120285511+GRS80@users.noreply.github.com>
2022-12-14 02:13:28 +0100

    fix: dockerfile

--- app/client/vite.config.js
+++ app/client/vite.config.js
@@ -45,7 +45,7 @@ export default defineConfig(({mode}) => {
     build: {
       target: 'es2021',
       cssTarget: 'chrome80',
-      chunkSizeWarningLimit: 500,
+      chunkSizeWarningLimit: 3000,
     },
     server: {
       host: '0.0.0.0',
