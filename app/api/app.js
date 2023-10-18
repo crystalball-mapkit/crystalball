d46349573541627c0d37c887373479e6a52588a1
Anatolii Suhanov <anatoly.i.sukhanov@gmail.com>
2023-10-18 19:51:49 +0700

    #64: add api endpoint for translating all features in table

--- app/api/app.js
+++ app/api/app.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 // Require dependencies
 const express = require("express");
 const logger = require("morgan");
@@ -14,8 +16,8 @@ const permissionController = require("./controllers/auth/permissionController.js
 const layerController = require("./controllers/gis/layerController.js");
 const uploadController = require("./controllers/upload/uploadController.js");
 const assetsController = require("./controllers/gis/assetsController.js");
-const configController = require("./controllers/other/configController.js")
-const translateController = require("./controllers/other/translateController.js")
+const configController = require("./controllers/other/configController.js");
+const translateController = require("./controllers/other/translateController.js");
 const upload = require("./services/file-upload.js");
 
 // Use middleware
@@ -23,15 +25,13 @@ app.use(logger("dev"));
 app.use(bodyParser.json({ limit: "10mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
 
-
-
 app
   .route("/api/register")
   .post(registerController.validate, registerController.register_post);
 
 app
   .route("/api/register_guest")
-  .post(registerController.validate, registerController.register_guest_post)
+  .post(registerController.validate, registerController.register_guest_post);
 
 app
   .route("/api/updateUserPassword")
@@ -53,10 +53,9 @@ app.route("/api/users").get(userController.users_get);
 app
   .route("/api/users/:id")
   .get(userController.users_get)
-  .patch(userController.user_patch)
+  .patch(userController.user_patch);
 
-app.route("/api/delete-user")
-  .post(userController.user_delete)
+app.route("/api/delete-user").post(userController.user_delete);
 
 app.route("/api/roles").get(roleController.roles_get);
 
@@ -80,7 +79,10 @@ app.route("/api/layer").post(layerController.layer_post);
 
 app.route("/api/upload").post(uploadController.file_upload);
 
-app.route("/api/translate").post(translateController.translate)
+app.route("/api/translate").post(translateController.translate);
+app
+  .route("/api/translate/:layer")
+  .get(translateController.translateAllFeatures);
 
 app
   .route("/api/html")
@@ -89,31 +91,24 @@ app
   .patch(assetsController.html_sidebar_patch)
   .post(assetsController.html_sidebar_post);
 
-
 app
   .route("/api/icons")
   .get(assetsController.icons_get)
   .post(assetsController.icons_post)
   .patch(assetsController.icons_patch);
 
-app
-  .route("/api/icons/:id")
-  .delete(assetsController.icons_delete);
+app.route("/api/icons/:id").delete(assetsController.icons_delete);
 
-app
-  .route("/api/icons/count-posts")
-  .post(assetsController.icons_count_posts);
+app.route("/api/icons/count-posts").post(assetsController.icons_count_posts);
 
 app
   .route("/api/icons/replace_posts")
   .post(assetsController.icons_replace_posts);
 
+app.route("/api/config").get(configController.config_get);
 
-app
-  .route("/api/config")
-  .get(configController.config_get);
-
-console.log(process.env.S3_BUCKET)
+console.log("POSTGRES_DB_NAME", process.env.POSTGRES_DB_NAME);
+console.log("S3_BUCKET", process.env.S3_BUCKET);
 // Run server on port
 // Run server on port
 app.listen(process.env.API_PORT || 3000, () =>
--- app/api/controllers/other/translateController.js
+++ app/api/controllers/other/translateController.js
@@ -1,30 +1,111 @@
 const permissionController = require("../auth/permissionController");
-const deepl = require('deepl-node');
+const sequelize = require("../../db.js");
+const deepl = require("deepl-node");
+const { translateContent } = require("../../utils");
+const jwt = require("jsonwebtoken");
+const jwtDecode = require("jwt-decode");
+const { v4: uuidv4 } = require("uuid");
 const translator = new deepl.Translator(process.env.DEEPL_API_KEY);
 
-const langVariants = { 
-    "en": "en-US",
-    "pt": "pt-BR"
+const langVariants = {
+  en: "en-US",
+  es: "es",
+  pt: "pt-BR",
 };
 
-exports.translate = (req, res) => {
-    
-    if (langVariants[req.body.targetLanguage]) {
-        req.body.targetLanguage = langVariants[req.body.targetLanguage];
-    }
+const languages = ["es", "pt"];
 
-    permissionController.hasPermission(req, res, "edit_html", () => {
-        translator
-            .translateText(req.body.content, req.body.sourceLanguage || null, req.body.targetLanguage, {
-                tagHandling: req.body.tagHandling || 'html',
-            })
-            .then((result) => {
-                res.status(200);
-                res.json(result.text);
-            })
-            .catch((error) => {
-                res.status(500);
-                res.json("Couldn't translate");
-            });
-    });
+function parseXmlToJson(xml) {
+  const json = {};
+  for (const res of xml.matchAll(
+    /(?:<(\w*)(?:\s[^>]*)*>)((?:(?!<\1).)*)(?:<\/\1>)|<(\w*)(?:\s*)*\/>/gm
+  )) {
+    const key = res[1] || res[3];
+    const value = res[2] && parseXmlToJson(res[2]);
+    json[key] = (value && Object.keys(value).length ? value : res[2]) || null;
+  }
+  return json;
 }
+
+exports.translate = (req, res) => {
+  if (langVariants[req.body.targetLanguage]) {
+    req.body.targetLanguage = langVariants[req.body.targetLanguage];
+  }
+
+  permissionController.hasPermission(req, res, "edit_html", () => {
+    translator
+      .translateText(
+        req.body.content,
+        req.body.sourceLanguage || null,
+        req.body.targetLanguage,
+        {
+          tagHandling: req.body.tagHandling || "html",
+        }
+      )
+      .then((result) => {
+        res.status(200);
+        res.json(result.text);
+      })
+      .catch((error) => {
+        res.status(500);
+        res.json("Couldn't translate");
+      });
+  });
+};
+
+exports.translateAllFeatures = async (req, res) => {
+  permissionController.hasPermission(req, res, "edit_layers", async () => {
+    if (req.params.layer) {
+      try {
+        const response = await sequelize.query(
+          `SELECT * FROM ${req.params.layer} WHERE translations IS NULL`
+        );
+        if (response[0].length > 0) {
+          for (const feature of response[0]) {
+            let translations = {};
+            let xml = "";
+            for (let key in feature) {
+              if (
+                ![
+                  "year",
+                  "amount",
+                  "funder",
+                  "start",
+                  "end",
+                  "variable1",
+                  "variable2",
+                  "geom",
+                  "translations",
+                ].includes(key)
+              ) {
+                xml += `<${key}>${feature[key]}</${key}>`;
+              }
+            }
+            for (const lang of languages) {
+              const translation = await translator.translateText(
+                xml,
+                "en",
+                langVariants[lang],
+                {
+                  tagHandling: "xml",
+                }
+              );
+              translations[lang] = parseXmlToJson(translation.text);
+            }
+            await sequelize.query(
+              `UPDATE ${req.params.layer} SET translations = $$${JSON.stringify(
+                translations
+              )}$$ WHERE id = $$${feature.id}$$;`
+            );
+          }
+        }
+        res.status(200);
+        return res.json();
+      } catch (err) {
+        console.log("err", err);
+        res.status(400);
+        res.json();
+      }
+    }
+  });
+};
