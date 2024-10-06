8634f639745bf8b30e5f840981de3b34baa284b7
grs80 <120285511+GRS80@users.noreply.github.com>
2024-10-06 15:25:58 +0200

    fix: #89 (wip)

--- app/client/src/utils/Helpers.js
+++ app/client/src/utils/Helpers.js
@@ -179,3 +179,17 @@ export function getHtml(content, defaultLanguage, currentLanguage) {
   }
   return html;
 }
+
+
+export function deepMerge(obj1, obj2) {
+  for (const key in obj2) {
+    if (obj2.hasOwnProperty(key)) {
+      if (obj2[key] instanceof Object && obj1[key] instanceof Object) {
+        obj1[key] = deepMerge(obj1[key], obj2[key]);
+      } else {
+        obj1[key] = obj2[key];
+      }
+    }
+  }
+  return obj1;
+}
